3a4cf52a23287b54a3f61d9b0d3fc3a0e1ff401d
FlorianSahbi <florian.sahbi@gmail.com>
2020-12-01 03:01:18 +0100

    Change endpoint

--- src/Client.tsx
+++ src/Client.tsx
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/",
-  // uri: "https://mindyourowntrip.com/graphql",
+  // uri: "http://localhost:4000/",
+  uri: "https://mindyourowntrip.com/graphql",
   cache: new InMemoryCache(),
 });
 
